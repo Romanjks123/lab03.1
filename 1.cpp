aa5ef36758fbeee6cc93a5630d1e1d7971ffb944
Romanjks123 <m7playyt@gmail.com>
2024-09-18 01:16:51 +0300

    first master coomit

---
+++ 1.cpp
@@ -0,0 +1,56 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+
+ int main() 
+{
+	double A;
+	double B;
+	double x;
+	double y;
+
+	cout << "x ="; cin >> x;
+
+	A = x;
+	if (x <= -1) {
+		B = log10(abs(cos(x * 5))) + pow(M_E, ((1 / x) + x));
+	}
+	
+	if (-1 <= x && x <= 0.4) {
+		B = sqrt(pow(2 - x, 3)) - tan(x);
+	}
+
+	if (x >= 0.4) {
+		B = sin(x * 5) - sqrt(abs(1 - x));
+	}
+	cout << endl;
+
+	y = A + B;
+	cout << "1) y=" << y << endl;
+
+
+
+
+	if (x <= -1) {
+		B = log10(abs(cos(x * 5))) + pow(M_E, ((1 / x) + x));
+
+	}
+	else
+	{
+		if (x >= 0.4) {
+			B = sin(x * 5) - sqrt(abs(1 - x));
+		}
+		else
+		{
+			B = sqrt(pow(2 - x, 3)) - tan(x);
+
+		}
+	}
+
+	y = A + B;
+	cout << "2) y=" << y << endl;
+	cin.get();
+	return 0;
+}
